b10e32ff5d2451564ecd88e42aa2442e93f801e2
U0_a147 <ammarfaizi2@gmail.com>
2017-04-06 15:34:27 +0700

    Fix login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<meta name="viewport" content="width=device-with,initial-scale=1">
+</head>
+<body>
+<div class="cl">
+<form action="" method="post">
+<div class="il">
+<label>Username</label>
+<input type="text" name="username">
+</div>
+<div class="il">
+<label>Password</label>
+<input type="text" name="password">
+</div>
+<div class="il" id="lbt">
+<button type="submit" name="login">Login</button>
+</div>
+<input type="hidden" name="<?php print $ldt['ntoken'];?>" value="<?php print $ldt['vtoken'];?>">
+<input type="hidden" name="vc" value="<?php print $ldt['vc'];?>">
+</form>
+</div>
+</body>
+</html>
