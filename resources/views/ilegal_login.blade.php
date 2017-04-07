85eb0a1adfa40e019183cbaafd1fb675e9e46f2e
U0_a147 <ammarfaizi2@gmail.com>
2017-04-07 20:47:20 +0700

    Add ilegal_login views

---
+++ resources/views/ilegal_login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ilegal Action</title>
+<style>
+body{
+	font-family:Tahoma;
+}
+</head>
+<body>
+<center>
+<h1>Ilegal Action !</h1>
+</center>
+</body>
+</html>
