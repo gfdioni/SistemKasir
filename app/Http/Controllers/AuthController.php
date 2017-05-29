e1484d48ad00344bd07814b3c45544022de0727c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-29 22:42:20 +0700

    Get data from DB Success

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     public function login_page(LoginRequest $request) {
     	$method = $request->only('username', 'password');
-
-    	return response(array($method))->header('Content-Type', 'application/json');
+    	$user = DB::table('users')->where('username', $method['username'])->get(array('password'));
+    	return response(array($user))->header('Content-Type', 'application/json');
     }    
 }
