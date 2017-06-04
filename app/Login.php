0f9a0bffb82a71e6855cdf21d589df3a6531a5a5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-04 14:57:32 +0700

    Create Login model

---
+++ app/Login.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use WhiteHat\Encryption\Teacrypt;
+
+class Login extends Model
+{
+    public function check_login($username, $password)
+    {
+
+    }
+}
